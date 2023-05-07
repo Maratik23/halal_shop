e72a0742f77485efa6bbd7e340640937a6394a90
RomanKurlygin <127237143+RomanKurlygin@users.noreply.github.com>
2023-05-07 16:58:17 +0300

    Added CSS + HTML for logic

---
+++ HalalDelivery/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace HalalDelivery.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int OrderId { get; set; }
+        
+        public string Username { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Address { get; set; }
+        
+    }
+}
